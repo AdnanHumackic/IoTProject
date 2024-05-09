7b767ff7c6a784bb224bea4b4613aeb5ed229ce7
Adnan Humačkić <humackic.adnan@gmail.com>
2024-05-09 17:59:58 +0200

    Mini change

--- IotProject_frontend/index.js
+++ IotProject_frontend/index.js
@@ -4,18 +4,13 @@ import { getDatabase, ref, onValue, get, set } from "https://www.gstatic.com/fir
 const firebaseConfig = {
     //your_firebase_config
 };
-
 const app = initializeApp(firebaseConfig);
 function getAllData() {
-    
     const db = getDatabase(app);
-    
     const dataRef = ref(db, "data");
-    
     onValue(dataRef, (snapshot) => {
     const data = snapshot.val();
     console.log("Updated data:", data);
-    
     if (data) 
     {
     console.log("Humidity:", data.Humidity);
@@ -23,8 +18,6 @@ function getAllData() {
     console.log("Temperature:", data.Temperature);
     console.log("Moisture Status:", data.MoistureStatus);
     console.log("Low Temp LED Status:", data.LowTempLedStatus);
-    
-              
     document.getElementById("temp").innerHTML = `${data.Temperature} &deg;C`;
     document.getElementById("airHumidity").innerHTML = `${data.Humidity} %`;
     document.getElementById("soilM").innerHTML = `${data.SoilMoisture} %`;
@@ -35,68 +28,50 @@ function getAllData() {
         });
 }
 getAllData();
-    
  window.turnOnWaterPump = async function() 
 {
     const db = getDatabase(app);
     const dataRef = ref(db, "data");
-
     const snapshot = await get(dataRef);
     const currentData = snapshot.val();
-    
     currentData.MoistureStatus = 1;
     
     await set(dataRef, currentData);
     document.getElementById("btnTwoOn").style.display="none"
     document.getElementById("btnTwoOff").style.display="inline"
     document.getElementById("fanStatus").innerHTML="Water pump is currently: ON!";
-    
 }
 window.turnOffWaterPump = async function() 
 {
     const db = getDatabase(app);
     const dataRef = ref(db, "data");
-  
     const snapshot = await get(dataRef);
     const currentData = snapshot.val();
-
     currentData.MoistureStatus = 0;
-
     await set(dataRef, currentData);
     document.getElementById("btnTwoOn").style.display="inline"
     document.getElementById("btnTwoOff").style.display="none"
     document.getElementById("fanStatus").innerHTML="Water pump is currently: OFF!";
-
 }
 
-
 window.turnOnTheFan = async function() {
-    
     const db = getDatabase(app);
     const dataRef = ref(db, "data");
-  
     const snapshot = await get(dataRef);
     const currentData = snapshot.val();
-
     currentData.LowTempLedStatus = 1;
-
     await set(dataRef, currentData);
     document.getElementById("btnOn").style.display="none";
     document.getElementById("btnOff").style.display="inline";
     document.getElementById("waterPumpStatus").innerHTML="Fan is currently: ON!";
-
-
 }
 
 window.turnOffTheFan = async function() {
     const db = getDatabase(app);
     const dataRef = ref(db, "data");
-  
     const snapshot = await get(dataRef);
     const currentData = snapshot.val();
-
     currentData.LowTempLedStatus = 0;
-
     await set(dataRef, currentData);
     document.getElementById("btnOn").style.display="inline";
     document.getElementById("btnOff").style.display="none";
